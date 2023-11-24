337d0ac1025a1f12b19df3e4186d2b1a45399eb5
williamqzy <qzhiyua@g.clemson.edu>
2023-11-24 08:50:38 -0800

    Create __init__.py

---
+++ settings/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+from .model_inputs import ModelFeatures, DataTypes, InputTypes, get_single_col_by_input_type, extract_cols_from_data_type
+# Import other modules and submodules
+# from .module_name import class_name, function_name
+
+__version__ = "0.1.0"  # Set your package version
